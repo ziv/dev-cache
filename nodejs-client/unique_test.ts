65f49abf45784b54371bf579243861e7c059fd32
ziv-perry <ziv.perry@zoominfo.com>
2025-01-15 17:52:58 +0200

    client tests

--- nodejs-client/unique_test.ts
+++ nodejs-client/unique_test.ts
@@ -2,10 +2,10 @@ import test from 'node:test';
 import assert from 'node:assert';
 import unique from './unique';
 
-test('unique() should return 40 long string', (t) => {
+test('unique() should return 40 long string', () => {
     assert.equal(unique('test').length, 40);
 });
 
-test('unique() should return the same output from the same input', (t) => {
+test('unique() should return the same output from the same input', () => {
     assert.equal(unique('test'), unique('test'));
 });
